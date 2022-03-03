9c8961dfa6649d5091cef169bfe95f404ecc321f
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2022-03-03 00:45:31 -0500

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Prajwal Dhatwalia on 2022-03-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Prajwal Dhatwalia on 2022-03-03.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
